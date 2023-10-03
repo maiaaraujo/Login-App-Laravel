c326a5cf4657104be7aa56a28d62c21ddaadc155
Maia Araújo <jessicamaiaaraujo@gmail.com>
2023-10-03 19:50:17 -0300

    home-page

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,35 +1,29 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" style="color: #9c89b8;">PROJETO LARAVEL</a>
+    <img src="{{ asset('images/Nyan-cat.png') }}" alt="Minha Imagem" width="130" height="60" class="mr-8">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" href="#" style="color: #9c89b8;">Home</a>
+          <a class="nav-link active" href="#" style="color: #9c89b8; font-weight: bold;">Home</a>
         </li>
         @auth
         <li class="nav-item">
-          <a class="nav-link" href="{{route('logout')}}" style="color: #9c89b8;">Logout</a>
+          <a class="nav-link" href="{{route('logout')}}" style="color: #9c89b8; font-weight: bold;">Logout</a>
         </li>
         @else
         <li class="nav-item">
-          <a class="nav-link active" href="{{route('login')}}" style="color: #9c89b8;">Login</a>
+          <a class="nav-link active" href="{{route('login')}}" style="color: #9c89b8; font-weight: bold;">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{route('registration')}}" style="color: #9c89b8;">Registration</a>
+          <a class="nav-link" href="{{route('registration')}}" style="color: #9c89b8; font-weight: bold;">Registration</a>
         </li>
         @endauth
-       
+
       </ul>
 
-      <span class="navbar-text" style="color: #9c89b8;">
-      <span>Hello</span>
-      @auth
-        {{auth()->user()->name}}
-      @endauth 
-      </span>
     </div>
   </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,18 @@
 @extends('layout')
 @section('title', "Home Page")
 @section('content')
-  
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="row-md-4 text-center">
+            <h1 style="color: #9c89b8;">Hello</h1>
+            <h2 style="color: #9c89b8;"> @auth
+                {{auth()->user()->name}}
+                @endauth
+            </h2>
+        </div>
+        <div class="row-md-4 text-center">
+            <img src="{{ asset('images/cat.png') }}" alt="Minha Imagem" width="300" height="300">
+        </div>
+    </div>
+</div>
 @endsection
